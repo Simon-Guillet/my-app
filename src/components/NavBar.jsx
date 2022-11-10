d4318ddcd9b2f86eb1e0344e911916b79e4b22e6
Simon Guillet <simon.guillet@free.fr>
2022-11-10 12:00:18 +0100

    testing

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { Button, View, Text } from "react-native"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { StatusBar } from "expo-status-bar"
+
+export function NavBar({ navigation }) {
+	return (
+		<View>
+			<Text>test</Text>
+		</View>
+	)
+}
